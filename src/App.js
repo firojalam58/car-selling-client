2c2e85abc695e924bd94fec3c048e323cf437607
Firoj Alam Raju <firojalamraju85@gmail.com>
2022-11-23 22:06:47 +0600

    create react project added react router dom

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { route } from './Pages/Routes/Routes/Routes';
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={route}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
